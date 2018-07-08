dd8170c832e7bbd6d3c5ab77c62f61655c15e90c
Samson Yuen <samsony@outlook.com>
2018-07-08 18:23:46 +0000

    add semantic-ui and ability to add blog posts

--- app.js
+++ app.js
@@ -8,6 +8,49 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+// MONGOOSE/MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+})
+
+
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+// ROUTES
+app.get("/", function(req, res){
+    res.redirect("/blogs");
+})
+
+app.get("/blogs", function(req, res){
+    Blog.find({}, function (err, blogs){
+        if (err){
+            console.log("ERROR!");
+        }
+        else {
+            res.render("index", {blogs: blogs});
+        }
+    })
+});
+
+app.get("/blogs/new", function(req, res){
+    res.render("new")
+});
+
+app.post("/blogs", function(req, res){
+    
+    Blog.create(req.body.blog, function(err, newBlog){
+        if (err){
+            res.render("new");
+        }
+        else {
+            res.redirect("/blogs");
+        }
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is running");
