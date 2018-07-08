ab6bcfc272c95d68ad8786302f4c6bf563d8e773
Samson Yuen <samsony@outlook.com>
2018-07-08 19:25:47 +0000

    add update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var bodyParser = require("body-parser"),
+methodOverride = require("method-override"),
 mongoose = require("mongoose"),
 express = require("express"),
 app = express();
@@ -7,6 +8,7 @@ mongoose.connect("mongodb://localhost:27017/blog-app", { useNewUrlParser: true }
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 // MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -14,7 +16,7 @@ var blogSchema = new mongoose.Schema({
     image: String,
     body: String,
     created: {type: Date, default: Date.now}
-})
+});
 
 
 
@@ -23,7 +25,7 @@ var Blog = mongoose.model("Blog", blogSchema);
 // ROUTES
 app.get("/", function(req, res){
     res.redirect("/blogs");
-})
+});
 
 app.get("/blogs", function(req, res){
     Blog.find({}, function (err, blogs){
@@ -41,6 +43,10 @@ app.get("/blogs/new", function(req, res){
 });
 
 app.post("/blogs", function(req, res){
+    if (!req.body.blog.body){
+        res.redirect("/blogs/new");
+        return;
+    }
     Blog.create(req.body.blog, function(err, newBlog){
         if (err){
             res.render("new");
@@ -61,8 +67,31 @@ app.get("/blogs/:id", function(req, res){
             res.render("show", {blog: foundBlog});
         }
     })
-})
+});
+
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if (err){
+            console.log(err);
+            res.redirect("/blogs");
+        }
+        else {
+            res.render("edit", {blog: foundBlog});
+        }
+    })
+});
+
+app.put("/blogs/:id", function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if (err){
+            res.redirect("/blogs");
+        }
+        else {
+            res.redirect("/");
+        }
+    })
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is running");
-})
+});
